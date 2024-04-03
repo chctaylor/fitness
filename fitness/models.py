8d82aff50b771c01f59bf5b197ba829deb01892b
Chase Taylor <chase@Chases-Space-MacBook-Pro.local>
2024-04-03 17:22:15 -0600

    Created BodyComposition model and script to import CSV data

--- fitness/models.py
+++ fitness/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class BodyComposition(models.Model):
+    date = models.DateTimeField()
+    weight = models.FloatField()
+    fat_mass = models.FloatField(blank=True, null=True)
+    bone_mass = models.FloatField(blank=True, null=True)
+    muscle_mass = models.FloatField(blank=True, null=True)
+    hydration = models.FloatField(blank=True, null=True)
+
+    def __str__(self):
+        return f"Date: {self.date} Weight: {self.weight}"
